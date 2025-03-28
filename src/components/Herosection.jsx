8214fc233cde91c41e23ff1deb1a401d9fa60a7c
Jesper <jefy0001@stud.kea.dk>
2025-03-28 10:26:52 +0100

    refactor hero section layout and styles; enhance visuals and structure

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -5,17 +5,32 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 const Herosection = () => {
     return (
         <section className="hero">
-            <div className="rigth">
-                <h1>The Perfect Moment Between Past And Future</h1>
+            <div className="right">
+                <h1 className="fat"> The Perfect Moment</h1>
+                <p>Between Past And Future</p>
                 <button>Buy Now</button>
-                <div>
-                    <IoIosArrowRoundBack />
-                    <p>1</p>
-                    <IoIosArrowRoundForward />
+                <div className="page">
+                    <IoIosArrowRoundBack className="arrow" />
+                    <p className="number">1</p>
+                    <IoIosArrowRoundForward className="arrow" />
                 </div>
             </div>
             <div className="left">
-
+                <div className="selected">
+                    <img className="big" src="/iwatchimg/navy.png" alt="navy" />
+                    <img className="palette" src="/iwatchimg/farvepalette.png" alt="palette" />
+                </div>
+                <div className="options">
+                    <div className="option1-container">
+                        <img className="option1" src="/iwatchimg/navy.png" alt="navy" />
+                    </div>
+                    <div className="option2-container">
+                        <img className="option2" src="/iwatchimg/ocean.png" alt="ocean" />
+                    </div>
+                    <div className="option3-container">
+                        <img className="option3" src="/iwatchimg/mint.png" alt="mint" />
+                    </div>
+                </div>
             </div>
         </section>);
 }
