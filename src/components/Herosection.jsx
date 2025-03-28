876071919a9e04ad3ddb35c7538a3537931d04e4
Jesper <jefy0001@stud.kea.dk>
2025-03-28 13:25:45 +0100

    add pink image and update hero section layout; adjust styles and text formatting

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -7,7 +7,7 @@ const Herosection = () => {
         <section className="hero">
             <div className="right">
                 <h1 className="fat"> The Perfect Moment</h1>
-                <p>Between Past And Future</p>
+                <p>Between Past And <br /> Future</p>
                 <button>Buy Now</button>
                 <div className="page">
                     <IoIosArrowRoundBack className="arrow" />
@@ -17,18 +17,22 @@ const Herosection = () => {
             </div>
             <div className="left">
                 <div className="selected">
-                    <img className="big" src="/iwatchimg/navy.png" alt="navy" />
-                    <img className="palette" src="/iwatchimg/farvepalette.png" alt="palette" />
+                    <div className="img-wrapper">
+                        <img className="big" src="/iwatchimg/navy.png" alt="navy" />
+                    </div>
+                    <div className="palette-wrapper">
+                        <img className="palette" src="/iwatchimg/farvepalette.png" alt="palette" />
+                    </div>
                 </div>
                 <div className="options">
                     <div className="option1-container">
                         <img className="option1" src="/iwatchimg/navy.png" alt="navy" />
                     </div>
-                    <div className="option2-container">
-                        <img className="option2" src="/iwatchimg/ocean.png" alt="ocean" />
-                    </div>
                     <div className="option3-container">
                         <img className="option3" src="/iwatchimg/mint.png" alt="mint" />
+                    </div>
+                    <div className="option2-container">
+                        <img className="option2" src="/iwatchimg/pink.png" alt="pink" />
                     </div>
                 </div>
             </div>
